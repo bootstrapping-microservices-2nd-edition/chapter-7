d4f07e1a63f4836ffa854a62eace07286581b2b6
Ashley Davis <ashley@codecapers.com.au>
2023-09-03 11:05:51 +1000

    Updated Terraform to 1.5.6 and the Azure provider to 3.71.0.

--- example-2/providers.tf
+++ example-2/providers.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.29.1"
+      version = "~> 3.71.0"
     }
   }
 
-  required_version = ">= 1.3.1"
+  required_version = ">= 1.5.6"
 }
 
 provider "azurerm" {
